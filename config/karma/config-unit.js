e1f5a564cecc8ec1a8f7810ea51894bf19bb9192
chrisguttandin <chrisguttandin@media-codings.com>
2017-05-17 03:20:54 +0200

    further increase browserNoActivityTimeout

--- config/karma/config-unit.js
+++ config/karma/config-unit.js
@@ -4,7 +4,7 @@ module.exports = (config) => {
 
         basePath: '../../',
 
-        browserNoActivityTimeout: 20000,
+        browserNoActivityTimeout: 30000,
 
         client: {
             mochaWebWorker: {
