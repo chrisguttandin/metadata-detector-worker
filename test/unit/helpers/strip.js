8b1f751e676ffaa8f600e9634fd090eb8f270132
Christoph Guttandin <chrisguttandin@media-codings.com>
2018-03-23 16:00:51 +0100

    test: fix typo in comment

--- test/unit/helpers/strip.js
+++ test/unit/helpers/strip.js
@@ -5,7 +5,7 @@ import { strip } from '../../../src/helpers/strip';
 describe('strip', () => {
 
     afterEach((done) => {
-        // @todo This is a optimistic fix to prevent the famous 'Some of your tests did a full page reload!' error.
+        // @todo This is an optimistic fix to prevent the famous 'Some of your tests did a full page reload!' error.
         setTimeout(done, 500);
     });
 
